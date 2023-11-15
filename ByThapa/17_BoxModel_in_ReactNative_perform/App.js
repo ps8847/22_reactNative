535fe8c9602b0589cd3255b5e3e9bc2acffbb86a
Prince Sharma <ps789650@gmail.com>
2023-11-15 22:09:25 +0530

    added flexbox tuto0ial

--- ByThapa/17_BoxModel_in_ReactNative_perform/App.js
+++ ByThapa/17_BoxModel_in_ReactNative_perform/App.js
@@ -3,13 +3,12 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import NetflixCard from "./src/components/NetflixCard";
 import DemoStyles from "./src/Screens/DemoStyles";
-import DemoFlexStyles from "./src/Screens/DemoFlexStyles";
 
 const App = () => {
 
   return (
   <View>
-<DemoFlexStyles/>
+<DemoStyles/>
   </View>
  )
 }
